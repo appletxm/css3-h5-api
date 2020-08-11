c315deb938756cfd051c2d47bc69752b9038eacf
appletxm <appletxm@163.com>
2020-08-11 17:24:27 +0800

    add fetch test samples

--- scripts/server-2.js
+++ scripts/server-2.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const app = express()
 const assetsRouter = require('./server-router-assets')
+const crosOriginControl = require('./server-cross-origin-control')
 const apiRouter = require('./server-router-api')
 const logger = require('./server-log')
+const cookieParser = require('cookie-parser')
 
 const port = 9000
 const host = '0.0.0.0'
+
+app.use(cookieParser())
+
+app.use(['/cross-origin-test/post'], (req, res) => {
+  crosOriginControl.handlePost(req, res, logger)
+})
+
+app.use(['/cross-origin-test/get'], (req, res) => {
+  crosOriginControl.handleGet(req, res, logger)
+})
+
+app.use(['/cross-origin-test/jsonp'], (req, res) => {
+  crosOriginControl.handleJsonP(req, res, logger)
+})
 
 app.use(['/api', '/app/v1', '/web'], (req, res) => {
   apiRouter(req, res, logger)
