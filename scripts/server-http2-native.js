623acc8323a3b7d8e897f2c86ed29627dc0d874f
tangxiumei <appletxm@163.com>
2020-10-28 15:35:21 +0800

    add media test

--- scripts/server-http2-native.js
+++ scripts/server-http2-native.js
@@ -2,7 +2,7 @@ const http2 = require('http2')
 const fs = require('fs')
 const path = require('path')
 const port = 3000
-const host = '127.0.0.1'
+const host = '0.0.0.0'
 const routerAssets = require('./server-router')
 const logger = require('./server-log')
 
