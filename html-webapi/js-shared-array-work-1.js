4967757d4bf464588c32f9c1e171a254ec802a05
tangxiumei <appletxm@163.com>
2020-12-17 18:02:25 +0800

    add url

--- html-webapi/js-shared-array-work-1.js
+++ html-webapi/js-shared-array-work-1.js
@@ -1,4 +1,31 @@
 var config = {}
+var offCanvas = new OffscreenCanvas(800, 400)
+var ctx = offCanvas.getContext('2d')
+var blob = null
+
+console.info(ctx)
+
+function getImageSuccess(blob) {
+  console.info(URL)
+  var url = URL.createObjectURL(blob)
+  console.info(url)
+}
+
+function getImage() {
+  // console.info(config)
+  var xhr = new XMLHttpRequest()
+
+  xhr.addEventListener('load', function(event) {
+    // console.info('======', event.target.response)
+    blob = new URL(event.target.response)
+  
+    getImageSuccess()
+  })
+
+  xhr.responseType = 'arraybuffer' // blob, arraybuffer
+  xhr.open('GET', config.imageUrl, true)
+  xhr.send()
+}
 
 function doCompress() {
   console.info(config)
@@ -10,7 +37,8 @@ self.addEventListener('message', function(event) {
 
   if (data.type === 'compress') {
     config = data.config
-    doCompress()
+    getImage()
+    // doCompress()
   }
 })
 
