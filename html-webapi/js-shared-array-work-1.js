637457efd85b64f90dafbf4d786e585d183e47f7
tangxiumei <appletxm@163.com>
2020-12-19 13:54:38 +0800

    add sharedArrayBuffer

--- html-webapi/js-shared-array-work-1.js
+++ html-webapi/js-shared-array-work-1.js
@@ -1,14 +1,90 @@
 var config = {}
-var offCanvas = new OffscreenCanvas(800, 400)
-var ctx = offCanvas.getContext('2d')
+var offCanvas = null
+var ctx = null
 var blob = null
+var bitMap = null
+var sourceType = null
+var stb = null
 
-console.info(ctx)
+// console.info(ctx)
 
-function getImageSuccess(blob) {
-  console.info(URL)
-  var url = URL.createObjectURL(blob)
-  console.info(url)
+function createCanvas(bitMap) {
+  offCanvas = new OffscreenCanvas(400, 500)
+  ctx = offCanvas.getContext('2d')
+
+  // console.info(offCanvas.width, offCanvas.height)
+
+  self.createImageBitmap(bitMap, 0, 0, bitMap.width, bitMap.height, {
+    // resizeWidth: 200,
+    resizeHeight: 200
+  }).then(res => {
+    bitMap = res
+    console.info(bitMap)
+
+    // offCanvas.width = bitMap.width
+    // offCanvas.height = bitMap.height
+    ctx.fillStyle = 'red'
+    ctx.arc(300, 300, 50, 20, 90)
+    ctx.closePath()
+    ctx.fill()
+
+    ctx.drawImage(bitMap, 0, 0, bitMap.width, bitMap.height, 0, 0, bitMap.width, bitMap.height)
+    // console.info(ctx.getImageData(0,0,200,100), offCanvas.toBlob, offCanvas.toDataURL)
+    offCanvas.convertToBlob({
+      type: sourceType,
+      quality: 1
+    }).then(res => {
+      console.info(res)
+
+      var url = URL.createObjectURL(res)
+
+      stb = new SharedArrayBuffer(res.size + 100)
+  
+      postMessage({
+        type: 'imageUrl',
+        url,
+        stb
+      })
+    }).catch(err => {
+      console.info('err:', err)
+    })
+  }).catch(err => {
+    console.info('err:', err)
+  })
+
+  // console.info(ctx.getImageData(0, 0, 200, 100))
+  // var imageData = ctx.getImageData(0, 0, 200, 100)
+  // var blob = new Blob([imageData.data], {type})
+  // var url = URL.createObjectURL(blob)
+  // postMessage({
+  //   type: 'imageUrl',
+  //   url
+  // })
+}
+
+function getBitMap(blob) {
+  self.createImageBitmap(blob).then(res => {
+    bitMap = res
+    // console.info(bitMap)
+    createCanvas(bitMap)
+  }).catch(error => {
+    console.info('error:', error)
+  })
+}
+
+function getImageSuccess(blob, type) {
+  // blob = new Blob([arrayBuffer], {type})
+  // var url = URL.createObjectURL(blob)
+  sourceType = type
+
+  console.info(blob)
+
+  getBitMap(blob)
+
+  // postMessage({
+  //   type: 'imageUrl',
+  //   url
+  // })
 }
 
 function getImage() {
@@ -17,12 +93,14 @@ function getImage() {
 
   xhr.addEventListener('load', function(event) {
     // console.info('======', event.target.response)
-    blob = new URL(event.target.response)
-  
-    getImageSuccess()
+    blob = event.target.response
+    // var arrayBuffer = event.target.response
+    // console.info(xhr.getResponseHeader('content-type'))
+    // console.info(xhr.getAllResponseHeaders())
+    getImageSuccess(blob, xhr.getResponseHeader('content-type'))
   })
 
-  xhr.responseType = 'arraybuffer' // blob, arraybuffer
+  xhr.responseType = 'blob' // blob, arraybuffer
   xhr.open('GET', config.imageUrl, true)
   xhr.send()
 }
@@ -41,5 +119,3 @@ self.addEventListener('message', function(event) {
     // doCompress()
   }
 })
-
-// postMessage('test')
